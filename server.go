d003a506af8f004c5ee24289e4f9c092942c8714
Kwisatz <drobanton@gmail.com>
2017-11-05 20:36:43 +0300

    it is only the begining

---
+++ server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	h "siteExample/handlers"
+	"time"
+)
+
+func runServer(addr string) {
+	mux := http.NewServeMux()
+
+	//import static data
+	stylesHandler := http.StripPrefix(
+		"/styles/",
+		http.FileServer(http.Dir("./static/styles")),
+	)
+	jsHandler := http.StripPrefix(
+		"/js/",
+		http.FileServer(http.Dir("./static/js")),
+	)
+	imgHandler := http.StripPrefix(
+		"/img/",
+		http.FileServer(http.Dir("./static/img")),
+	)
+	mux.Handle("/styles/", stylesHandler)
+	mux.Handle("/js/", jsHandler)
+	mux.Handle("/img/", imgHandler)
+
+	//create pages
+	mux.HandleFunc("/", h.Root)
+	mux.HandleFunc("/test/", h.Test)
+
+	server := http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+
+	fmt.Println("Starting server at", addr)
+	server.ListenAndServe()
+}
+
+func main() {
+	fmt.Println("Main starts")
+	runServer(":8080")
+}
